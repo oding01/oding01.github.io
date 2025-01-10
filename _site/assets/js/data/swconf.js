93123b394943f2b5efc0dd20a81f2553e16c8311
이어진 <slimin92@naver.com>
2025-01-10 15:33:19 +0900

    avatar 오류 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736487762',resources: [
+    cacheName: 'chirpy-1736490501',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
