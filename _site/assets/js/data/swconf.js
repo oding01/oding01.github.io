b38f4d61765bd60a4c781685a68322cb5d43f37f
이어진 <slimin92@naver.com>
2025-01-12 05:21:15 +0900

    자잘한 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1736623147',resources: [
+    cacheName: 'chirpy-1736626857',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
